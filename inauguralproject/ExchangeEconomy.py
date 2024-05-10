37e12461b8f20dab0bf969f89eeb42bedaf9f6a4
XNQ881 <163315472+XNQ881@users.noreply.github.com>
2024-05-10 21:58:11 +0200

    Add files via upload

---
+++ inauguralproject/ExchangeEconomy.py
@@ -0,0 +1,49 @@
+from types import SimpleNamespace
+import numpy as np
+
+class ExchangeEconomyClass:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # a. preferences
+        par.alpha = 1/3
+        par.beta = 2/3
+
+        # b. endowments
+        par.w1A = 0.8
+        par.w2A = 0.3
+
+    def utility_A(self, x1A, x2A):
+        return x1A**self.par.alpha * x2A**(1 - self.par.alpha)
+
+    def utility_B(self, x1B, x2B):
+        return x1B**self.par.beta * x2B**(1 - self.par.beta)
+
+    def demand_A(self, p1):
+        p2 = 1  # Numeraire
+    
+        x1A = self.par.alpha / p1 * (self.par.w1A + p1/p2 * self.par.w2A)
+        x2A = (1 - self.par.alpha) / p2 * (self.par.w1A + p1/p2 * self.par.w2A)
+        return x1A, x2A
+
+    def demand_B(self, p1):
+        p2 = 1  # Numeraire
+        w1B = 1 - self.par.w1A  # Endowment of B for good 1
+        w2B = 1 - self.par.w2A  # Endowment of B for good 2
+        x1B = self.par.beta / p1 * (w1B + p1/p2 * w2B)
+        x2B = (1 - self.par.beta) / p2 * (w1B + p1/p2 * w2B)
+        return x1B, x2B
+
+    def check_market_clearing(self, p1):
+
+        par = self.par
+
+        x1A, x2A = self.demand_A(p1)
+        x1B, x2B = self.demand_B(p1)
+
+        eps1 = x1A - par.w1A + x1B - (1 - par.w1A)
+        eps2 = x2A - par.w2A + x2B - (1 - par.w2A)
+
+        return eps1, eps2
